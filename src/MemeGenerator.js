47b38d27e230bdd88f6df578fba9c6a1f8de2877
June Muoti <june.muoti@gmail.com>
2019-09-24 22:35:40 +0300

    add header and memegenerator components

---
+++ src/MemeGenerator.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+class MemeGenerator extends React.Component{
+    constructor(){
+        super()
+        this.state=
+        {}
+    }
+    render(){
+        return(
+            <div>
+                <h2>Meme Generator Section</h2>
+                </div>
+        )
+    }
+}
+
+
+export default MemeGenerator
